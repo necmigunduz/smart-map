158c4d75ed144efd801d8ef3569dfceaa620748d
necmigunduz <necmigunduz@gmail.com>
2022-11-01 10:48:11 +0300

    Add pulldown to refresh functionality and comments in code

--- components/quote.js
+++ components/quote.js
@@ -1,21 +1,42 @@
-import React, {useState, useEffect} from 'react';
-import {View, Button, Text, TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  RefreshControl,
+  View,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import {getQuote} from '../redux/quoteSlice';
 import {StyleSheet} from 'react-native';
 
+// Creating wait function to end spinning when a user pulls down to refresh
+const wait = timeout => {
+  return new Promise(resolve => setTimeout(resolve, timeout));
+};
+
 const Quote = () => {
+  // Setting quote and its author to be published
   const [quote, setQuote] = useState('');
   const [auth, setAuth] = useState('');
+  // Setting state of refreshing for RefreshControl
+  const [refreshing, setRefreshing] = useState(false);
+  // Accessing state through Redux Toolkit store
   const {quotes} = useSelector(state => state.quotes);
+  // Definee dispatch function to call actions from quoteSlice
   const dispatch = useDispatch();
-  useEffect(() => {
+  // Function to refresh quote upon a pulldown
+  const onRefresh = () => {
+    setRefreshing(true);
     dispatch(getQuote());
     setQuote(quotes.content);
     setAuth(quotes.author);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [quote, auth]);
-
+    console.log('quote');
+    // Stopping spin
+    wait(300).then(() => setRefreshing(false));
+  };
+  // Button to refresh quote
   const handleButton = () => {
     dispatch(getQuote());
     setQuote(quotes.content);
@@ -24,28 +45,30 @@ const Quote = () => {
   };
 
   return (
-    <View>
-      <View>
+    <SafeAreaView>
+      <ScrollView
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }>
         <Text style={styles.title}>Quote of Day</Text>
         <TouchableOpacity onPress={handleButton} style={styles.button}>
           <Text style={styles.btnText}>Update</Text>
         </TouchableOpacity>
         {quote ? (
           <View style={styles.card}>
-            <Text style={styles.dashes}>----------------</Text>
             <Text style={styles.text}>"{quote}"</Text>
             <Text style={styles.auth}>{auth}</Text>
-            <Text style={styles.dashes}>----------------</Text>
           </View>
         ) : (
           <Text style={styles.loading}>
             Click on Update to see day of quote
           </Text>
         )}
-      </View>
-    </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
+// Styles
 const styles = StyleSheet.create({
   button: {
     alignItems: 'center',
@@ -71,6 +94,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#bbe4e9',
     marginHorizontal: 20,
     borderRadius: 50,
+    borderColor: '#5585b5',
+    borderWidth: 4,
+    borderStyle: 'dotted',
+    padding: 30,
   },
   title: {
     backgroundColor: '#5585b5',
